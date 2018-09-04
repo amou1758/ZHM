f2d4a79398bfcac5e7c97a08a81e63501cb45628
amou1758 <amou1758@163.com>
2018-09-05 01:24:05 +0800

    3. 博客的数据库表结构!

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    
+class Tag(models.Model):
+    """
+    标签 Tag 也比较简单，和 Category 一样。
+    再次强调一定要继承 models.Model 类！
+    """
+    name = models.CharField(max_length=100)
+    
+    
+class Post(models.Model):
+    """
+    文章的数据库表稍微复杂一点，主要是涉及的字段更多。
+    """
+
+    # 文章标题
+    title = models.CharField(max_length=70)
+    # 文章正文
+    body = models.TextField()
+    # 创建时间和最后一次修改时间
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    # 文章摘要
+    excerpt = models.CharField(max_length=200, blank=True)
+    # 分类与标签
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    # 文章作者
+    # django.contrib.auth 是 Django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是 Django 为我们已经写好的用户模型。
+    # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
+    # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
+    author = models.ForeignKey(User)

95aedc870fd8171208a0578b2c91272ff72eda46
amou1758 <amou1758@163.com>
2018-09-05 04:38:10 +0800

    11. 归档与分类!

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ def index(request):
     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
+# 博文详情页
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.body = markdown.markdown(post.body,
@@ -17,3 +18,18 @@ def detail(request, pk):
                                       'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+# 归档
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+# 分类
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})

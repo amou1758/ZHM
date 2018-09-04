9e8b266c7ef0197e0d5a6ba3724d735d3846db8d
amou1758 <amou1758@163.com>
2018-09-05 03:04:14 +0800

    8. 博客文章详情页!

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+
+# 视图函数命名空间
+app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]

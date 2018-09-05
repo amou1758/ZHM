7afaf5be58dc9baf0c693853a3ec8de8fb9bb156
amou1758 <amou1758@163.com>
2018-09-05 21:44:09 +0800

    15. 统计文章阅读量!

--- blog/models.py
+++ blog/models.py
@@ -42,6 +42,8 @@ class Post(models.Model):
     # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和 Category 类似。
     author = models.ForeignKey(User)
+    # 新增views字段记录阅读量
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title
@@ -55,6 +57,10 @@ class Post(models.Model):
         :return:
         """
         return reverse('blog:detail', kwargs={'pk': self.pk})
+    
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
     
     class Meta:
         ordering = ['-created_time', 'title']

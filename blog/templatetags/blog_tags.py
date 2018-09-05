3ea0e0a8a5c355e62feb4661f0645b19fa15bb31
amou1758 <amou1758@163.com>
2018-09-06 02:23:03 +0800

    21. 标签云!

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -21,3 +21,9 @@ def get_categories():
     # 别忘了在顶部引入 Category 类
     return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
     # return Category.objects.all()
+
+
+@register.simple_tag
+def get_tags():
+    # 记得在顶部引入 Tag model
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
--- blog/views.py
+++ blog/views.py
@@ -299,4 +299,13 @@ class PostDetailView(DetailView):
         return context
 
 
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
+
 
